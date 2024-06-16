ed653f8bdb2dfe4332b53c910cacf010818c5794
SAMIUL BASIR <samiulb25@gmail.com>
2024-06-17 04:42:59 +0600

    Update the code finding nemo.

--- FindNemo.js
+++ FindNemo.js
@@ -1,17 +1,20 @@
 const nemo = ["nemo"]
 
-const everyone = ["barko", "jaro", "Sharko", "Drinko", "Boltu", "nemo", "kungfu"]
+const everyone = ["barko", "jaro", "Sharko", "Drinko", "Boltu", "nemo", "kungfu","fish", "cat", "rat"]
 
 const large = new Array(100).fill("nemo")
 
 function findNemo(array){
 
-     let t_0 = performance.now()
+    let t_0 = performance.now()
 
     for(let i = 0; i < array.length; i++){
 
+        console.log("Running!!!");
+
         if(array[i] === 'nemo'){
-            console.log("Hooray!, we found nemo!")
+            console.log("Hooray!, we found nemo!");
+            break;
         }
         // else{
         //     console.log("Sorry, we cannot find nemo!")
@@ -22,4 +25,6 @@ function findNemo(array){
     console.log(`Finding nemo takes time ` + (t_1 - t_0 ) + ` milliseconds` )
 }
 
-findNemo(large) // O(n) --> Linear Time
+// findNemo(large) // O(n) --> Linear Time
+
+findNemo(everyone);
