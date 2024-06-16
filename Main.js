bbae964a3ab59e70948115d6d27f07e5acfe269a
SAMIUL BASIR <samiulb25@gmail.com>
2024-06-17 04:32:24 +0600

    Just initiate the code.

--- Main.js
+++ Main.js
@@ -1,3 +1,26 @@
+function anotherFunction(input){
+
+    let a = 5;
+    let b = 10;
+    let c = 50;
+
+    for(let i = 0; i < input.length; i++){
+
+        let x = i + 1;
+        let y = i + 1;
+        let z = i + 1;
+
+    }
+
+    for(let j = 0; j < input.length; j++){
+
+        let p = j * 2;
+        let q = j * 2;
+    }
+
+    let whoAmI = "I don'nt know";
+}
+
 function funChallenge(input){
     let a = 10; 
     a = 50 + 3;
