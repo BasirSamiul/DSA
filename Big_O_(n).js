04b221e1d1605e3a2e31d397cbd6a89ed655e39c
SAMIUL BASIR <samiulb25@gmail.com>
2024-06-17 03:47:23 +0600

    Syntax of Big o(n) notation and how to use this formulas.

---
+++ Big_O_(n).js
@@ -0,0 +1,12 @@
+const compressAllBoxes = boxes => {
+    boxes.forEach(box => console.log(box));
+}
+
+// By using ES5
+// function compressAllBoxes(boxes){
+//     boxes.forEach(function(items){
+//         console.log(items);
+//     })
+// }
+const items = ['fish', 'fruits', 'grocery'];
+compressAllBoxes(items);
