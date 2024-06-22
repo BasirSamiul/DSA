fe2ae2915912e370a12a514e41f478802e23222e
SAMIUL BASIR <samiulb25@gmail.com>
2024-06-23 05:44:39 +0600

    Demonstrate another big o notation which faster than other code.

--- CommonItems_Part_One.js
+++ CommonItems_Part_One.js
@@ -1,2 +1,28 @@
 const array_1 = ['a', 'b','c'];
-const array_2 = ['x', 'y', 'w'];
+const array_2 = ['x', 'y', 'a'];
+
+function containsCommonItem(array_one, array_two){
+// Loop through first array and create object where properties === items in the array
+
+    let map = {};
+    for (let i = 0; i < array_one.length; i++){
+        if(!map[array_1[i]]){
+            const item  = array_1[i];
+            map[item] = true;
+        }
+    }
+
+//Loop through second array and check if item in second array exists on created object.
+    for(let j = 0; j < array_two.length; j++){
+        if(map[array_2[j]]){
+            return true;
+        }
+    }
+    return false;
+
+}
+
+const result = containsCommonItem(array_1, array_2);
+console.log(result);
+
+//this code big o is: Big O(n)
