664ba4d3f235baed048a2c825eaa35fa50bfa582
SAMIUL BASIR <samiulb25@gmail.com>
2024-06-24 09:19:20 +0600

    create the method push and pop and test by code.

--- Array/Myarray.js
+++ Array/Myarray.js
@@ -6,7 +6,23 @@ class MyArray{
     get (index){
         return this.data[index];
     }
+    push(item){
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+    pop(){
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
 }
 
 const newArray = new MyArray();
+newArray.push("hi");
+newArray.push("you");
+newArray.push("Out!!!");
+newArray.pop();
+newArray.pop();
 console.log(newArray);
