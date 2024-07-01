f934501d40b4b3cf7ea087ae67bcbc42346f00f7
SAMIUL BASIR <samiulb25@gmail.com>
2024-07-02 03:24:30 +0600

    Just create a user function

---
+++ Hash/user.js
@@ -0,0 +1,11 @@
+let user = {
+    age : 54,
+    name : "Kylie",
+    magic : true,
+    scream : function(){
+        console.log("Ahhhhhhh")
+    }
+}
+user.spell = "I Love Bangladesh.";
+
+console.log(user);
