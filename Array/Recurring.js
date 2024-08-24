86e8355fd59613a2e23cfc9513864125ee5d9688
SAMIUL BASIR <samiulb25@gmail.com>
2024-08-25 02:02:44 +0600

    Recurring a number from an array.

---
+++ Array/Recurring.js
@@ -0,0 +1,16 @@
+function firstRecurringCharacter(input){
+    for (let i = 0; i < input.length; i++){
+        for (let j = 1; j < input.length; j++){
+            if (input[i] === input[j]){
+                return input[i];
+            }
+        }
+    }
+    return undefined;
+}
+
+let ma_arr = [2,5,1,2,3,5,1,2,4];
+
+const result = firstRecurringCharacter(ma_arr);
+
+console.log(result);
