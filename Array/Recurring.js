f7e5c058e81b92add807e00e7039386fb3726eb9
SAMIUL BASIR <samiulb25@gmail.com>
2024-08-25 17:55:01 +0600

    Create next recurring number

--- Array/Recurring.js
+++ Array/Recurring.js
@@ -14,3 +14,18 @@ let ma_arr = [2,5,1,2,3,5,1,2,4];
 const result = firstRecurringCharacter(ma_arr);
 
 console.log(result);
+
+// Start another recurring character
+function nextRecurringCharacter(input){
+    let map = {};
+    for (let i = 0; i < input.length; i++){
+        if(map[input[i]]){
+            return input[i];
+        } else {
+            map[input[i]] = i;
+        }
+        console.log(map[input[i]]);
+        return undefined;
+    }
+}
+nextRecurringCharacter(ma_arr);
