b8889ef849c2b5d3a22f644862b9297517e83ad5
SAMIUL BASIR <samiulb25@gmail.com>
2025-01-16 19:03:55 +0600

    Linked List append, and prepend

--- LinkedList/Linked_first.js
+++ LinkedList/Linked_first.js
@@ -8,7 +8,28 @@ class LinkedList{
         this.length = 1;
 
     }
+    append(value){
+        const newNode = {
+            value : value,
+            next : null
+        }
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+    prepend(value){
+        const newNode = {
+            value : value,
+            next : this.head
+        }
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
 }
 
 const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.prepend(20);
 console.log(myLinkedList);
